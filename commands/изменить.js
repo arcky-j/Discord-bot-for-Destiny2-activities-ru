3ed2ea574862b18cc3363aad0caefdd02f5f8df7
yaraglebow@gmail.com <yaraglebow@gmail.com>
2023-08-24 12:50:07 +0700

    небольшая новая команда

---
+++ commands/изменить.js
@@ -0,0 +1,32 @@
+const {SlashCommandBuilder, PermissionFlagsBits} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+            .setName('изменить')
+            .setDescription('Управление профилем пользователя')
+            .addSubcommand(subcommand =>
+                subcommand.setName('ник')
+                .setDescription('Ещё один способ изменить ник на сервере')
+                    .addStringOption(option =>
+                        option.setName('никнейм')
+                            .setDescription('Никнейм, который вы хотите установить на этом сервере')
+                            .setRequired(true))),
+
+    async execute (interaction){
+        const user = interaction.member;
+        
+        if (interaction.options.getSubcommand() === 'ник'){
+            const newNick = interaction.options.getString('никнейм');
+            user.setNickname(newNick)
+            .then(() => {
+                const embed = interaction.client.genEmbed(`Ник изменён!`, 'Успех!');
+                interaction.reply({embeds: [embed], ephemeral:true});
+            })
+            .catch((err) => {
+                const embed = interaction.client.genEmbed(`${err.message}`, 'Ошибка!');
+                interaction.reply({embeds: [embed], ephemeral:true});
+            });                
+        }
+     
+    }
+};
